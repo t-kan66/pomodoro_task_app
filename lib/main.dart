15e57aafce0462ffe0f927169530b82080e7711a
t.k <55534352+t-kan66@users.noreply.github.com>
2025-01-05 22:32:28 +0900

    firebaseの環境構築

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app/myapp.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   const flavor = String.fromEnvironment('flavor');
   print('flavor: $flavor');
+
+  await Firebase.initializeApp().then((value) {
+    log('Firebase.initializeApp() completed ${value.options.appId}');
+  });
 
   runApp(const MyApp());
 }
