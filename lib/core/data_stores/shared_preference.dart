c8ece9d0f8330af72411d3ce5b2fb68b86826af0
t.k <55534352+t-kan66@users.noreply.github.com>
2025-03-25 21:05:17 +0900

    preferenceの追加

--- lib/core/data_stores/shared_preference.dart
+++ lib/core/data_stores/shared_preference.dart
@@ -1,14 +1,20 @@
-
 // shared preference のdata store
 import 'package:shared_preferences/shared_preferences.dart';
+
+// preference名を列挙
+class PreferenceKeys {
+  static const String workDuration = 'work_duration'; // 作業時間
+  static const String breakDuration = 'break_duration'; // 休憩時間
+  static const String phaseCount = 'phase_count'; // フェーズ数
+}
 
 class SharedPreferenceDataStore {
   final SharedPreferences _sharedPreferences;
 
   SharedPreferenceDataStore(this._sharedPreferences);
 
-  // ユーザー名を取得
-  String getUserName() {
+  // SharedPreferenceを取得
+  String setTimerSetting(String keyString) {
     return _sharedPreferences.getString('user_name') ?? '';
   }
 
