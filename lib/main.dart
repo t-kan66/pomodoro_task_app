d6dbe7cbae17d6981229919b78f91aeb22cf9b80
t.k <55534352+t-kan66@users.noreply.github.com>
2025-06-11 06:55:51 +0000

    firebaseの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'dart:developer';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app/myapp.dart';
+import 'firebase_options_dev.dart' as dev_options;
+import 'firebase_options_prod.dart' as prod_options;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,9 +15,15 @@ void main() async {
   const flavor = String.fromEnvironment('flavor');
   print('flavor: $flavor');
 
-  await Firebase.initializeApp().then((value) {
+  final firebaseOptions =
+      flavor == 'prod' ? prod_options.DefaultFirebaseOptions.currentPlatform : dev_options.DefaultFirebaseOptions.currentPlatform;
+
+  await Firebase.initializeApp(options: firebaseOptions).then((value) {
     log('Firebase.initializeApp() completed ${value.options.appId}');
   });
+
+  // Firebase Analyticsの初期化
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
   runApp(const ProviderScope(child: MyApp()));
 }
